89593dc6213dbaf1476172d1ee43197341cca9df
long <1072362251@qq.com>
2022-03-05 09:33:21 +0800

    123

---
+++ js/reg.js
@@ -0,0 +1,61 @@
+window.onload = function() {
+    var regtel = /^1[3|4|5|7|8]\d{9}$/; // 手机号码的正则表达式
+    var regqq = /^[1-9]\d{4,}$/; // 10000
+    var regnc = /^[\u4e00-\u9fa5]{2,8}$/;
+    var regmsg = /^\d{6}$/;
+    var regpwd = /^[a-zA-Z0-9_-]{6,16}$/;
+    var tel = document.querySelector('#tel');
+    var qq = document.querySelector('#qq');
+    var nc = document.querySelector('#nc');
+    var msg = document.querySelector('#msg');
+    var pwd = document.querySelector('#pwd');
+    var surepwd = document.querySelector('#surepwd');
+    regexp(tel, regtel); // 手机号码
+    regexp(qq, regqq); // qq号码
+    regexp(nc, regnc); // 昵称
+    regexp(msg, regmsg); // 短信验证
+    regexp(pwd, regpwd); // 密码框
+    // 表单验证的函数
+    // function regexp(ele, reg) {
+    //     ele.onblur = function() {
+    //         if (reg.test(this.value)) {
+    //             // console.log('正确的');
+    //             this.nextElementSibling.className = 'success';
+    //             this.nextElementSibling.innerHTML = '<i class="success_icon"></i> 恭喜您输入正确';
+    //         } else {
+    //             // console.log('不正确');
+    //             this.nextElementSibling.className = 'error';
+    //             this.nextElementSibling.innerHTML = '<i class="error_icon"></i> 格式不正确，请从新输入 ';
+    //         }
+    //     }
+    // };
+     // 实时表单验证的函数
+    function regexp(ele, reg) 
+    {
+        // oninput此事件会在value属性值发生改变时触发，通过js改变value属性值不会触发此事件。
+        // 只有IE8以上或者谷歌火狐等标准浏览器支持。
+        ele.addEventListener("input",function(){
+            if (reg.test(this.value)) {
+                // console.log('正确的');
+                this.nextElementSibling.className = 'success';
+                this.nextElementSibling.innerHTML = '<i class="success_icon"></i> 恭喜您输入正确';
+            } else {
+                // console.log('不正确');
+                this.nextElementSibling.className = 'error';
+                this.nextElementSibling.innerHTML = '<i class="error_icon"></i> 格式不正确，请从新输入 ';
+            }
+        })
+        
+    };
+    surepwd.oninput = function() {
+        if (this.value == pwd.value) {
+            this.nextElementSibling.className = 'success';
+            this.nextElementSibling.innerHTML = '<i class="success_icon"></i> 恭喜您输入正确';
+        } else {
+            this.nextElementSibling.className = 'error';
+            this.nextElementSibling.innerHTML = '<i class="error_icon"></i> 两次密码输入不一致';
+
+        }
+    }
+
+}
